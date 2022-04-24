6f88664067bd4a7516ad34f458ddfb31c71c15ba
Deepjyoti Bora <59271987+deepbora@users.noreply.github.com>
2022-04-24 20:10:26 +0530

    modify 1.test.js

--- 1test.js
+++ 1test.js
@@ -1,1 +1,20 @@
 console.log('Hello world');
+
+
+// var v = 10;
+// var v = 70;
+
+
+// console.log(v);
+// console.log(typeof(v));
+
+// var k = 10 + "20";
+// var k = 10 + 20;
+// var k = "java" + "strings";
+// var k = "  " + "  ";
+// var k = "java" - "strings";
+// var k = true + true;
+// var k = true + false;
+var k = false - true;
+console.log(k);
+console.log(typeof(k));
